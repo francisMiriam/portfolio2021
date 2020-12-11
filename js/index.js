8280448327bd0090bd6950779fa5b9de75932db5
Manuel Stieglitz-Hartmann <manuel.hartmann@mailbox.org>
2020-12-11 13:14:31 +0100

    feat: adds ScrollReveal for animation

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import ScrollReveal from 'scrollreveal'
+
+const options = {
+  delay: 250,
+  distance: '20px'
+}
+
+ScrollReveal(options);
+
+ScrollReveal().reveal('.francis-portrait')
+ScrollReveal().reveal('.image')
+ScrollReveal().reveal('.image-vita')
