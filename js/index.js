0383faef49b0338ebc0177cfdc65b72cd96cd6a4
Manuel Stieglitz-Hartmann <manuel.hartmann@mailbox.org>
2020-12-16 19:44:50 +0100

    feat: improves Text

--- js/index.js
+++ js/index.js
@@ -1,12 +1,22 @@
 import ScrollReveal from 'scrollreveal'
 
 const options = {
-  delay: 250,
-  distance: '20px'
+  delay: 700,
+  opacity: 0,
+  // distance: '100px',
+  scale: 0.9,
+  duration: 900,
+  easing: 'cubic-bezier(0.5, 0, 0, 0.5)',
 }
 
 ScrollReveal(options);
 
-ScrollReveal().reveal('.francis-portrait')
+ScrollReveal().reveal('.francis-portrait',{
+  delay: 100,
+  // opacity: 0,
+  // distance: '20px',
+  duration: 300,
+  easing: 'cubic-bezier(0.5, 0, 0.25, 0.5)',
+} )
 ScrollReveal().reveal('.image')
 ScrollReveal().reveal('.image-vita')
